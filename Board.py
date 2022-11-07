e0ccf33b3b897470b4cd439b0111b8f304d31ce6
jared-bauman <45722664+jared-bauman@users.noreply.github.com>
2022-11-06 20:40:13 -0800

    Create Board.py

---
+++ Board.py
@@ -0,0 +1,25 @@
+class Board:
+  def __init__(self, N, WIN_PATTERNS):
+    self.N = N
+    self.WIN_PATTERNS = WIN_PATTERNS
+    self.board = np.array([0]*N*N) # board representation
+    self.active_pos = np.array([0]*N) # column position counter
+
+  def check_win(self):
+    for s in WIN_PATTERNS:
+      t = np.sum(self.board[s])
+      if t==4:
+        return 1
+      elif t==-4:
+        return -1
+    return 0
+
+  def place(self, x, player_one):
+    assert 0<=x<self.N
+    y = active_pos[x]
+    assert 0<=y<N
+    board[(self.N-1)-y+x*self.N] = (1 if player_one else -1)
+    active_pos[x] += 1
+
+  def print_board(self):
+    print(np.transpose(np.reshape(board,(self.N,self.N))))
