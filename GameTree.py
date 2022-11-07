27545964a11c0bc79bcc4409f3d8111e69fa42e7
jared-bauman <45722664+jared-bauman@users.noreply.github.com>
2022-11-06 21:34:57 -0800

    Update GameTree.py

--- GameTree.py
+++ GameTree.py
@@ -17,7 +17,7 @@ class GameTree:
     # add state to game tree if not already there
     c = board.tobytes()
     if c not in self.idxs:
-      self.idxs[c] = len(idxs)
+      self.idxs[c] = len(self.idxs)
       self.children.append(np.array([-1]*N))
       self.values.append(np.array([0.,0]))
 
@@ -25,9 +25,26 @@ class GameTree:
     decay = 1.0
     visit_idxs.reverse()
     for i in visit_idxs:
-      c = self.values[i][COUNT_IDX]
-      self.values[i][VALUE_IDX] = (
-          (self.values[i][VALUE_IDX] * c + w * decay) / (c + 1)
+      c = self.values[i][GameTree.COUNT_IDX]
+      self.values[i][GameTree.VALUE_IDX] = (
+          (self.values[i][GameTree.VALUE_IDX] * c + w * decay) / (c + 1)
       )
-      self.values[i][COUNT_IDX] += 1
+      self.values[i][GameTree.COUNT_IDX] += 1
       decay *= gamma
+      
+  def update_values_td0(self, visit_idxs, w, alpha=0.05, gamma=1.0):
+    visit_idxs.reverse()
+    
+    # update the game end state value
+    s_prime = visit_ids[-1]
+    self.values[s_prime][GameTree.VALUE_IDX] = w
+    self.values[s_prime][GameTree.COUNT_IDX] += 1
+    
+    # update all other values with bootstrap
+    for i in range(len(visit_idxs)-1):
+      s = visit_idxs[i]
+      self.values[s][GameTree.VALUE_IDX] += alpha * (
+          gamma*self.values[s_prime][GameTree.VALUE_IDX] - self.values[s][GameTree.VALUE_IDX]
+      )
+      self.values[s][GameTree.COUNT_IDX] += 1
+      s_prime = s
